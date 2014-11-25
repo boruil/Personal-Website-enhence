824104526f01fa7c8aa30b589cd3896353ceb128
Andy <boruil@cs.washington.edu>
2014-11-25 12:29:35 -0800

    modify email functions

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'boruil@cs.washington.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'boruil@cs.washington.edu';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: boruil@cs.washington.edu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
